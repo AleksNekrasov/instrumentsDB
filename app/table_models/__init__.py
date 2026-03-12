b78aea244dc0ab3b9d04f23d284d721a5e5a33bb
alex_nout <nsk540010@gmail.com>
2026-03-12 23:20:17 +0700

    5 commit. Создал БД при помощи alembic

---
+++ app/table_models/__init__.py
@@ -0,0 +1,7 @@
+from .employee import Employee
+from .location import Location
+from .repair import Repair
+from .tool import Tool
+from .tool_issue import ToolIssue
+from .tool_model import ToolModel
+from .tool_movement import ToolMovement
