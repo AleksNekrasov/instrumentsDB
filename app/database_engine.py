bf6287a4cd0c44c1304450fcf7a488ce3918ee04
alex_nout <nsk540010@gmail.com>
2026-03-28 18:04:39 +0700

    9 commit.  немного коррекции, добавил .env файл

--- app/database_engine.py
+++ app/database_engine.py
@@ -1,11 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import DeclarativeBase
 
 DATABASE_URL = "sqlite:///instruments.db"
 engine = create_engine(url=DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
 
+
+#---------Асинхронное подключение к PostgreSQL----------------------
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from decouple import config
+
+# Строка подключения для PostgreSQl
+DATABASE_URL = config("DATABASE_URL")
+
+# Создаем Асинхронный движок
+async_engine = create_async_engine(url=DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов(сессий)
+async_session_maker = async_sessionmaker(bind=async_engine,expire_on_commit=False, class_=AsyncSession)
+
+# Создаем базовый класс
 class Base(DeclarativeBase):
     pass
 
